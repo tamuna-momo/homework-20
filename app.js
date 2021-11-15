7fa5fc6b74c06e2abec4920cdd8d6a3b23801e87
tamuna-momo <tamuna-momo@users.noreply.github.com>
2021-11-15 13:49:26 +0400

    pop-up and get data from server_task 1

---
+++ app.js
@@ -0,0 +1,58 @@
+const userModalId = '#user-form-modal';
+function modal(modalId){
+    const modalWrapper = document.querySelector(modalId);
+    const modalContent = modalWrapper.querySelector('.modal-content');
+    const closeBtn= modalContent.querySelector('.close');
+
+    modalWrapper.style.display='block';
+
+    modalContent.addEventListener('click', e => {
+        e.stopPropagation();
+      });
+      modalWrapper.addEventListener('click', e => {
+        modalWrapper.style.display = 'none';
+      });
+     closeBtn.addEventListener('click', e => {
+     modalWrapper.style.display='none';
+})
+closeBtn.addEventListener('click', e => {
+    modalWrapper.style.display = 'none';
+});
+function close(){
+    modalWrapper.style.display = 'none';
+  }
+
+  return {
+    close
+  }
+}
+
+const modalButtons= document.querySelectorAll('[data-modal-target]');
+
+modalButtons.forEach((btn) => {
+    btn.addEventListener('click', e =>{
+        console.log(e.target.dataset.modalTarget);
+           modal(e.target.dataset.modalTarget);
+    })
+
+})
+console.log(modalButtons);
+
+// 1.
+function getUsers(){
+    fetch('http://api.kesho.me/v1/user-test/index')
+    .then(resp => {
+     return resp.json();
+    })
+    .then(data=>{
+        console.log(data);
+    }). catch(error=>{
+        console.log(error);
+    })
+        
+}
+getUsers();
+  
+
+
+
